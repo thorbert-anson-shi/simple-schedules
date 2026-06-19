19c25151bd0279f93469d5da31cacf68d700325f
thorbert-anson-shi <tobyas2005139@gmail.com>
2026-06-19 21:45:29 +0700

    fix: added setup for e2e tests

---
+++ test/setup-env.js
@@ -0,0 +1,6 @@
+process.env.DATABASE_URL = 'postgresql://admin:replace-with-secure-password@localhost:5432/simple-schedules';
+process.env.JWT_SECRET = "jwt secret let's hope this is safe";
+process.env.ADMIN_EMAIL = 'admin@staging.com';
+process.env.ADMIN_PASSWORD = 'adminpassword123';
+process.env.ADMIN_LOOKAHEAD_DAYS = '180';
+process.env.CUSTOMER_LOOKAHEAD_DAYS = '14';
